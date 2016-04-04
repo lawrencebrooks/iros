62f679bfc5651a3186577b5abd744bcdd478e2e8
Lawrence Brooks <lawrence@impactradius.com>
2016-04-04 15:15:28 +0200

    Added instrument patches

--- src/data/patches.h
+++ src/data/patches.h
@@ -19,108 +19,43 @@
 
 #define PATCH_NAVIGATE 0
 
-const char ad_navigate[] PROGMEM ={
-	0,PC_WAVE,2,
-	0,PC_NOTE_DOWN,50, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_UP,1, 
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_DOWN,1,
-	1,PC_NOTE_CUT,0,
-	0,PATCH_END
-};
+  //INST: lead fast env       Channel 1....
+  const char patch00[] PROGMEM ={ 
+	  0,PC_WAVE,4,
+	  0,PC_ENV_SPEED,-35,
+	  0,PATCH_END
+  };
 
-const struct PatchStruct my_patches[] PROGMEM = {
-  {0,0,ad_navigate,0,0},
-};
+  //INST: lead slow env       Channel 2....
+  const char patch01[] PROGMEM ={ 
+	  0,PC_WAVE,4,
+	  0,PC_ENV_SPEED,-10,
+	  0,PATCH_END
+  };
+
+  //INST: bass -  triangular  Channel 3...
+  const char patch02[] PROGMEM ={	
+	  0,PC_WAVE,2,
+	  1,PC_NOTE_HOLD,0,
+	  1,PC_ENV_SPEED,-32,
+	  0,PATCH_END
+  };
+
+  //INST: hi-hat              Channel 4...
+  const char patch03[] PROGMEM ={	
+	  0,PC_NOISE_PARAMS,1,
+	  0,PC_ENV_VOL,0x90,
+	  0,PC_ENV_SPEED,-30, 
+	  8,PC_NOTE_CUT,0,
+	  0,PATCH_END
+  }; 
+
+  const struct PatchStruct my_patches[] PROGMEM = {
+	  {0,NULL,patch00,0,0},
+	  {0,NULL,patch01,0,0},
+	  {0,NULL,patch02,0,0},
+	  {1,NULL,patch03,0,0},
+  };
+
 
 #endif
